ec0b4b4ffcd156006e9b10601e5f0a03263979ec
ctrlfir3 <59097667+ctrlfir3@users.noreply.github.com>
2019-12-20 15:48:55 -0500

    Add files via upload

---
+++ app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LeftBoxComponent } from "./components/left-box/left-box.component";
+import { LeftMainPageComponent } from "./components/left-box/left-main-page/left-main-page.component";
+import { RightBoxComponent } from "./components/right-box/right-box.component";
+import { LeftPage2Component } from "./components/left-box/left-page2/left-page2.component";
+import { RightMainPageComponent } from "./components/right-box/right-main-page/right-main-page.component";
+import { RightPage2Component } from "./components/right-box/right-page2/right-page2.component";
+import { LeftBoxRoutingModule } from './components/left-box/left-box-routing.module';
+import { RightBoxRoutingModule } from './components/right-box/right-box-routing.module';
+
+const routes: Routes = [
+  { path: "main", component: LeftBoxComponent },
+  { path: "", redirectTo: "main", pathMatch: "full" }
+];
+
+@NgModule({
+  imports: [
+    LeftBoxRoutingModule,
+    RightBoxRoutingModule,
+    RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
